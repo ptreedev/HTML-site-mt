bd305069a24869b5803e7d4f5a59995f8ace059a
Peter Leaver <peteleaver@madetech.com>
2025-07-17 14:27:34 +0100

    Added tests for checking content

--- python-bottle-site/tests/test_index.py
+++ python-bottle-site/tests/test_index.py
@@ -1,6 +1,14 @@
 from python_bottle_site.python_bottle_site import index
 
-def test_smoke_index_render():
-    component = index()
-    assert component is not None
-    
+class Test_Index_Content:
+    index_component = index()
+    rendered_dict = index_component.render()
+    sub_components = rendered_dict["children"]
+    def test_smoke_index_render(self):
+        assert self.index_component is not None
+    def test_component_container_exists(self):
+        assert self.rendered_dict["name"] == "RadixThemesContainer"
+    def test_component_vstack_exists_in_container(self):
+        vstack = next((child for child in self.sub_components if child["name"] == "RadixThemesFlex"), None)
+        assert vstack is not None, "VStack should be present"
+
